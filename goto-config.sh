deb5ea1b122f325ccb7c878477fa9eb3b26e7c13
Timofey Asyrkin <tasyrkin@gmail.com>
2015-04-08 08:57:36 +0200

    Added goto-config.sh

---
+++ goto-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Specify parent directories where the gogo scripts fetches data from
+GOTO_PARENT_DIRECTORIES=(~/Projects)
+#Specify prefixes to exclude from names
+REMOVE_PREFIXES=("zeos-" "zalando-")
+
+function remove_prefix() {
+  result=$1
+  for remove_prefix in ${REMOVE_PREFIXES[*]}; do
+    result=${result#$remove_prefix}
+  done
+  echo $result
+}
+
+for parent_dir in ${GOTO_PARENT_DIRECTORIES[*]}; do
+
+  for goto_dir_name in $( ls $parent_dir ); do
+  
+    goto_full_path="$parent_dir/$goto_dir_name"
+    
+    if [[ -d $goto_full_path ]]; then
+      goto_target=$( remove_prefix $goto_dir_name )
+      alias goto-$goto_target="cd $goto_full_path"
+    fi
+  done
+done
